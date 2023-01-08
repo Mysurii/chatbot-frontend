0e86a1ffd97095ff68041d450e8e8814c84e073f
Ali Ozcan <ali.ozcan@hva.nl>
2023-01-08 16:24:18 +0100

    fixed network err

--- index.js
+++ index.js
@@ -14,152 +14,151 @@ const STANDARD_AVATAR_URL =
 
 function createChatbotLayout() {
   // chat box
-  const chatBox = document.createElement("div");
+  const chatBox = document.createElement('div')
   chatBox.classList.add('chatbot__container')
-  chatBox.id = "chat-box";
+  chatBox.id = 'chat-box'
 
   //header
-  const header = document.createElement("div");
+  const header = document.createElement('div')
   header.classList.add('chatbot__header')
-  
-  header.style.background = headerColor;
 
-  const closeBtn = document.createElement("div");
-  closeBtn.innerHTML =  '&#10006';
+  header.style.background = headerColor
+
+  const closeBtn = document.createElement('div')
+  closeBtn.innerHTML = '&#10006'
   closeBtn.classList.add('chatbot__header__close')
-  closeBtn.style.color = closeColor;
+  closeBtn.style.color = closeColor
 
-  closeBtn.addEventListener("click", () => {
-    avatar.style.opacity = 1;
-    chatBox.style.opacity = 0;
-  });
+  closeBtn.addEventListener('click', () => {
+    avatar.style.opacity = 1
+    chatBox.style.opacity = 0
+  })
 
-  const chatbotNameSpan = document.createElement("span");
+  const chatbotNameSpan = document.createElement('span')
   chatbotNameSpan.classList.add('chatbot__header__description')
-  chatbotNameSpan.innerHTML = chatbotName || "Chatbot";
-  chatbotNameSpan.style.color = nameColor;
+  chatbotNameSpan.innerHTML = chatbotName || 'Chatbot'
+  chatbotNameSpan.style.color = nameColor
 
-  header.appendChild(chatbotNameSpan);
-  header.appendChild(closeBtn);
+  header.appendChild(chatbotNameSpan)
+  header.appendChild(closeBtn)
 
   //body
-  const messagesListContainer = document.createElement("div");
-  messagesListContainer.classList.add('chatbot__messageslist__container');
+  const messagesListContainer = document.createElement('div')
+  messagesListContainer.classList.add('chatbot__messageslist__container')
 
-  const chatLogs = document.createElement("div");
-  chatLogs.classList.add("chatbot__messageslist");
+  const chatLogs = document.createElement('div')
+  chatLogs.classList.add('chatbot__messageslist')
 
-  messagesListContainer.appendChild(chatLogs);
+  messagesListContainer.appendChild(chatLogs)
 
   // Start message
-  createMessage(
-    "Hello, I am the virtual assistant of this website. How can I help you?",
-    true
-  );
+  createMessage('Hello, I am the virtual assistant of this website. How can I help you?', true)
   createMessage('Hello, I am the virtual assistant of this website. How can I help you?', false)
   // Input
-  const inputContainer = document.createElement("form");
+  const inputContainer = document.createElement('form')
   inputContainer.classList.add('chatbot__input__container')
-  const inputField = document.createElement("input");
-  inputField.classList.add("chatbot__input");
-  inputField.placeholder = "Type message..";
+  const inputField = document.createElement('input')
+  inputField.classList.add('chatbot__input')
+  inputField.placeholder = 'Type message..'
 
-  const sendButton = document.createElement("button");
-  sendButton.type = "submit";
-  sendButton.innerText = "Send";
+  const sendButton = document.createElement('button')
+  sendButton.type = 'submit'
+  sendButton.innerText = 'Send'
   sendButton.classList.add('chatbot__send__button')
-  sendButton.style.color = closeColor;
+  sendButton.style.color = `${closeColor} !important`
 
-  sendButton.addEventListener("click", async (e) => {
-    e.preventDefault();
+  sendButton.addEventListener('click', async (e) => {
+    e.preventDefault()
 
-    const message = inputField.value;
-    if (message === "") return;
-    createMessage(message);
-    inputField.value = "";
-    scrollBottom();
-    inputField.focus();
+    const message = inputField.value
+    if (message === '') return
+    createMessage(message)
+    inputField.value = ''
+    scrollBottom()
+    inputField.focus()
 
-    const response = await getResponse(message);
-
-    if (response.status == "success")
+    try {
+      const response = await getResponse(message)
+      if (response.status == 'success')
+        setTimeout(() => {
+          createMessage(response.message, true)
+          scrollBottom()
+        }, 500)
+    } catch (err) {
       setTimeout(() => {
-        createMessage(response.message, true);
-        scrollBottom();
-      }, 500);
-  });
+        createMessage('Something went wrong with the server. Please try again later.', true)
+        scrollBottom()
+      }, 500)
+    }
+  })
 
-  inputContainer.appendChild(inputField);
-  inputContainer.appendChild(sendButton);
+  inputContainer.appendChild(inputField)
+  inputContainer.appendChild(sendButton)
 
-  chatBox.appendChild(header);
-  chatBox.appendChild(messagesListContainer);
-  chatBox.appendChild(inputContainer);
+  chatBox.appendChild(header)
+  chatBox.appendChild(messagesListContainer)
+  chatBox.appendChild(inputContainer)
 
+  const avatar = document.createElement('div')
+  avatar.classList.add('avatar__container')
+  const avatarImg = document.createElement('img')
+  avatarImg.src = avatarURL || STANDARD_AVATAR_URL
+  avatarImg.classList.add('chatbot__avatar')
+  avatar.appendChild(avatarImg)
 
-  const avatar = document.createElement("div");
-  avatar.classList.add("avatar__container");
-  const avatarImg = document.createElement("img");
-  avatarImg.src = avatarURL || STANDARD_AVATAR_URL;
-  avatarImg.classList.add("chatbot__avatar");
-  avatar.appendChild(avatarImg);
+  avatarImg.addEventListener('click', () => {
+    avatar.style.opacity = 0
+    chatBox.style.opacity = 1
+  })
 
-  avatarImg.addEventListener("click", () => {
-    avatar.style.opacity = 0;
-    chatBox.style.opacity = 1;
-  });
-
-  document.body.appendChild(chatBox);
-  document.body.appendChild(avatar);
+  document.body.appendChild(chatBox)
+  document.body.appendChild(avatar)
 
   const scrollBottom = () =>
     chatLogs.scrollTo({
       top: chatLogs.scrollHeight,
-      behavior: "smooth",
-    });
+      behavior: 'smooth',
+    })
 
   function createMessage(message, is_bot = false) {
-    const container = document.createElement("div");
+    const container = document.createElement('div')
     container.classList.add('chatbot__bubble__container')
-    container.style.justifyContent = is_bot ? "flex-start" : "flex-end";
+    container.style.justifyContent = is_bot ? 'flex-start' : 'flex-end'
 
-    const textContainer = document.createElement("div");
+    const textContainer = document.createElement('div')
     textContainer.classList.add('chatbot__text__container')
 
-    const bubble = document.createElement("div");
-    bubble.classList.add(is_bot ? 'chatbot__bot__bubble' : "chatbot__self__bubble");
+    const bubble = document.createElement('div')
+    bubble.classList.add(is_bot ? 'chatbot__bot__bubble' : 'chatbot__self__bubble')
 
-    bubble.append(message);
+    bubble.append(message)
 
-    const time = document.createElement("div");
-    time.classList.add('chatbot__time');
-    time.style.textAlign = is_bot ? "left" : "right";
+    const time = document.createElement('div')
+    time.classList.add('chatbot__time')
+    time.style.textAlign = is_bot ? 'left' : 'right'
 
     const now = new Date()
     now.innerText = `${now.getHours()}:${now.getMinutes()}`
 
     textContainer.appendChild(bubble)
-    textContainer.appendChild(time);
+    textContainer.appendChild(time)
 
-    container.appendChild(textContainer);
-    chatLogs.appendChild(container);
+    container.appendChild(textContainer)
+    chatLogs.appendChild(container)
   }
 }
 
 async function getResponse(message) {
-  const response = await fetch(
-    `http://localhost:5000/api/chatbot/response/${chatbotName}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message }),
-    }
-  );
-  const res = await response.json();
+  const response = await fetch(`http://localhost:8080/api/chatbot/response/${chatbotName}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message }),
+  })
+  const res = await response.json()
 
-  return res;
+  return res
 }
 
 // requestStylesheet(
